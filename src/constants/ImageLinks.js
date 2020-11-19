6a46c466cf5f9ea6d52518afce996a264e268ab3
Dominic Go <dominicstop@gmail.com>
2020-11-19 23:45:04 +0800

    🖼 Added 2014/2015 Images

--- src/constants/ImageLinks.js
+++ src/constants/ImageLinks.js
@@ -6,14 +6,10 @@ export const Images2014 = Helpers.range(1, 136).map(i => (
   `https://github.com/dominicstop/happy-birthday-regina/blob/master/assets/2014%20Images/2014-${i}.jpg?raw=true`
 ));
 
-export const Images2015 = Helpers.range(0, 0).map(i => (
+export const Images2015 = Helpers.range(1, 129).map(i => (
   `https://github.com/dominicstop/happy-birthday-regina/blob/master/assets/2015%20Images/2015-${i}.jpg?raw=true`
 ));
 
-export const Images2016 = Helpers.range(0, 0).map(i => (
+export const Images2016 = Helpers.range(1, 11).map(i => (
   `https://raw.githubusercontent.com/dominicstop/happy-birthday-regina/master/assets/2016%20Images/2016-${i}.jpg`
-));
-
-export const Images2017 = Helpers.range(0, 0).map(i => (
-  `https://github.com/dominicstop/happy-birthday-regina/blob/master/assets/2017%20Images/2017-${i}.jpg?raw=true`
 ));
